275bb16a77f317f305324fc82e38b34d067e7c5a
ibrahimshittu <ibshittu01@gmail.com>
2022-11-07 00:05:57 +0100

    array types

--- SectionThree/app.ts
+++ SectionThree/app.ts
@@ -1,19 +1,28 @@
 // const person: {
 //     name: string;
 //     age: number;
+//     hobbies: string[];
+//     role: [number, string]; tuple
 // } = {
 //     name: 'Maximilian',
 //     age: 30
 // };
 
+enum Role { ADMIN = 3, READ_ONLY = "READ_ONLY", AUTHOR = 7 };
+
 const person = {
     name: 'Maximilian',
     age: 30,
-    hobbies: ['Sports', 1, true]
+    hobbies: ['Sports', 1, true],
+    role: Role.ADMIN
 };
 
 console.log(person.age)
 
 for (let i = 0; i < person.hobbies.length; i++) {
     console.log(person.hobbies[i]);
+}
+
+for (const hobby of person.hobbies) {
+    console.log(hobby);
 }
