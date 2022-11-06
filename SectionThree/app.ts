064d079fc3f2abbd1439061426cecb4ed7054110
ibrahimshittu <ibshittu01@gmail.com>
2022-11-06 22:36:37 +0100

    working with numbers, strings & booleans

--- SectionThree/app.ts
+++ SectionThree/app.ts
@@ -1,10 +1,15 @@
 
-function addNumber(n1: number, n2: number) {
-    return n1 + n2
+function addNumber(n1: number, n2: number, printResult: boolean) {
+    if (printResult) {
+        console.log(n1 + n2);
+    } else {
+        return n1 + n2
+    }
 }
 
 const number = 1;
 const number2 = 2.8;
+const showResult = true;
 
-const result = addNumber(number, number2);
+const result = addNumber(number, number2, showResult);
 console.log(result);
