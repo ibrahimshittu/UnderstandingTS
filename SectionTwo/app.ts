532165546575b5508a0312a5804f855a49cab58a
ibrahimshittu <ibshittu01@gmail.com>
2023-03-05 11:21:17 +0100

    function return

--- SectionTwo/app.ts
+++ SectionTwo/app.ts
@@ -1,1 +1,10 @@
-console.log('Your code goes here...');
+function add(n1: number, n2: number): number {
+    return n1 + n2
+}
+
+function printResult(num: number) {
+    console.log("result: " + num)
+
+}
+
+printResult(add(5, 12))
