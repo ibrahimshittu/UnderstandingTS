2b1d6cd2d6f6c6cb77c003baeca5efb77851012c
ibrahimshittu <ibshittu01@gmail.com>
2023-03-05 11:33:56 +0100

    function types and callback

--- SectionTwo/app.ts
+++ SectionTwo/app.ts
@@ -8,6 +8,11 @@ function printResult(num: number): void {
 
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2
+    cb(result)
+}
+
 printResult(add(5, 12))
 
 // combines values should take a function that accepts two numbers, and return a number
@@ -16,3 +21,5 @@ let combinedValues: (a: number, b: number) => number;
 combinedValues = add;
 
 console.log(combinedValues(8, 8))
+
+addAndHandle(2, 7, (result) => console.log(result))
