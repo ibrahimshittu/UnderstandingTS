6ccd630431dd49efac9f6b29eeed56e5f2b0832a
ibrahimshittu <ibshittu01@gmail.com>
2023-03-05 12:10:23 +0100

    function types and callback

--- SectionTwo/app.ts
+++ SectionTwo/app.ts
@@ -1,5 +1,5 @@
 let userInput: unknown;
-// let userInput: any;
+// let userInput: any; this does not offer any type checking 
 let username: string;
 
 userInput = 5;
@@ -7,3 +7,13 @@ userInput = 'Ibrahim';
 if (typeof userInput === 'string') {
     username = userInput
 }
+
+// a function that will never return anything, be it undefined or null
+function generateError(message: string, code: number): never {
+    throw {
+        message: message,
+        errorCode: code
+    }
+}
+
+generateError('An Error occurred', 500)
