7f9c57770dc0e9c8310edeffaf74838d2bad6465
ibrahimshittu <ibshittu01@gmail.com>
2023-03-05 11:27:41 +0100

    function types

--- SectionTwo/app.ts
+++ SectionTwo/app.ts
@@ -2,9 +2,17 @@ function add(n1: number, n2: number): number {
     return n1 + n2
 }
 
-function printResult(num: number) {
+// void return type has no return statement
+function printResult(num: number): void {
     console.log("result: " + num)
 
 }
 
 printResult(add(5, 12))
+
+// combines values should take a function that accepts two numbers, and return a number
+let combinedValues: (a: number, b: number) => number;
+
+combinedValues = add;
+
+console.log(combinedValues(8, 8))
