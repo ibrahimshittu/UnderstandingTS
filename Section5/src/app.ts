3887f154fc3d0b086bca0e55260ac854767ca08d
ibrahimshittu <ibshittu01@gmail.com>
2023-03-05 21:22:47 +0100

    Classes & interfaces- getters and setters

--- Section5/src/app.ts
+++ Section5/src/app.ts
@@ -1,6 +1,6 @@
 class Department {
     // private name: string;
-    private employees: string[] = [] // you can also add methods as private 
+    protected employees: string[] = [] // you can also add methods as private 
 
     constructor(private readonly name: string, public id: string) {
 
@@ -20,11 +20,57 @@ class Department {
     }
 }
 
+class ITDepartment extends Department {
+    constructor(id: string, public admins: string[]) {
+        super(id, "IT");
+    }
+}
+class AccountingDepartment extends Department {
+    private reports: string[]
+    lastReport: string;
+
+    get mostRecentReport() {
+        if (this.lastReport) {
+            this.lastReport
+        }
+        throw new Error("no Report found!")
+    }
+
+    set mostRecentReport(value: string) {
+        if (!value) {
+            throw new Error("pass in value!")
+        }
+        this.addAndPrintReport(value)
+    }
+
+    constructor(id: string, reports: string[]) {
+        super(id, "Accounting");
+        this.reports = reports;
+        this.lastReport = reports[0];
+    }
+
+    addEmployee(name: string) {
+        if (name === "max") {
+            return
+        }
+        this.employees.push(name)
+    }
+
+    addAndPrintReport(text: string) {
+        this.reports.push(text);
+        this.lastReport = text;
+        console.log("This is the text: " + text)
+    }
+}
+
 const ibrahim = new Department("Ibrahim", "a1");
+const itDepartment = new ITDepartment("Ibrahim", ["a1"]);
+new AccountingDepartment("a1", ["hello"]).mostRecentReport = 'lates report'
 
 ibrahim.describe();
 ibrahim.addEmployee("Ibrahim Shittu")
 ibrahim.printEmployeeInformation()
+
 
 // ibrahim.employees[2] = "Sofiu" #this would not work due to th eprivate keyword in front of employees key
 
