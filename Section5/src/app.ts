528741d9f9ae072881c758e17a6604228c8a501f
ibrahimshittu <ibshittu01@gmail.com>
2023-03-05 19:41:26 +0100

    Classes & interfaces

--- Section5/src/app.ts
+++ Section5/src/app.ts
@@ -1,9 +1,33 @@
 class Department {
-    name: string;
-    constructor(n: string) {
-        this.name = n;
+    // private name: string;
+    private employees: string[] = [] // you can also add methods as private 
 
+    constructor(private readonly name: string, public id: string) {
+
+    }
+
+    describe(this: Department) {
+        console.log("Department: " + this.name + this.id)
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length)
+        console.log(this.employees)
     }
 }
 
-new Department("Ibrahim");
+const ibrahim = new Department("Ibrahim", "a1");
+
+ibrahim.describe();
+ibrahim.addEmployee("Ibrahim Shittu")
+ibrahim.printEmployeeInformation()
+
+// ibrahim.employees[2] = "Sofiu" #this would not work due to th eprivate keyword in front of employees key
+
+// const ibrahimCopy = { name: "HELLO", describe: ibrahim.describe }
+
+// ibrahimCopy.describe()
